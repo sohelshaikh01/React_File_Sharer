2547361ea60f57acfe6d55fb5915498d600951cd
Sohel Shaikh <sohel.techin@gmail.com>
2026-04-29 12:00:51 +0530

    Improve Loading style

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,21 +1,30 @@
 import { Navigate, Outlet } from "react-router-dom";
 import useAuth from "../context/useAuth.js";
-import { useEffect } from "react";
 
 const ProtectedRoute = () => {
   const { user, loading } = useAuth();
 
-  useEffect(() => {
-  }, [user]);
-
   // ⏳ wait until auth is loaded
   if (loading) {
-    return <p className="text-center mt-10 text-gray-400">Loading...</p>;
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-gray-950">
+        <div className="flex flex-col items-center gap-4">
+          
+          {/* Spinner */}
+          <div className="w-12 h-12 border-4 border-gray-700 border-t-blue-500 rounded-full animate-spin"></div>
+
+          {/* Text */}
+          <p className="text-gray-400 text-sm tracking-wide">
+            Checking authentication...
+          </p>
+
+        </div>
+      </div>
+    );
   }
 
   // 🔐 check auth
   return user ? <Outlet /> : <Navigate to="/login" replace />;
-  
 };
 
 export default ProtectedRoute;
